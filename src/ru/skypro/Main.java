0557975b237002c799e29566600da9302dcc959a
Андрей <79538233369@ya.ru>
2022-05-29 19:13:14 +0500

    Homework 7.1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,31 +1,16 @@
 package ru.skypro;
 
 public class Main {
-    public static void printSeparator() {
-        System.out.println("+++++++");
-        System.out.println("-------");
-    }
-    public static void printIssues(int issueCount) {
-        System.out.println(issueCount);
-    }
-    public static int sum (int [] numbers) {
-        int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
-            sum = sum + numbers[i];
-        }
-        return sum;
+    public static boolean leapOrNot(int year) {
+        boolean leap = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
+        if (leap) {
+            System.out.println("The year " + year + " is leap");
+        }else {
+            System.out.println("The year " + year + " is not leap");}
+        return leap;
     }
     public static void main(String[] args) {
-        int[] issuesByMonths = {4, 6, 7, 9, 2, 5, 12, 3, 7, 10, 6, 7, 1, 8};
-                printSeparator();
-        for (int i = 0; i < issuesByMonths.length; i++) {
-            printIssues(issuesByMonths[i]);
-            if ((i+1)%3 == 0) {
-                printSeparator();
-            }
-        }
-        printSeparator();
-        int total = sum(issuesByMonths);
-        printIssues(total);
-    }
-}
+        System.out.println("Homework 7.1");
+        int year = 5100;
+        boolean checkYear = leapOrNot(year);
+    }}
